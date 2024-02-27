6349f5cc406e10e17e15f2857b7ad47cd758b280
blesera <161520579+blesera@users.noreply.github.com>
2024-02-28 09:44:14 +1300

    Add files via upload

---
+++ PO_2_variables.py
@@ -0,0 +1,11 @@
+# code to learn about variables
+
+#v ariables can be a sting (note the speech marks)
+greeting= "hello world"
+
+# variables can be a number
+fav_num= 1
+num_string = "5"
+
+print(fav_num * 2)
+print(num_string * 5)   
